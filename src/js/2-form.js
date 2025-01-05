0de06c6ca4dba643bb2d8d4cd1df78ad4486687a
Vlad Bokii <bokiyvladik10@gmail.com>
2025-01-05 20:04:23 +0200

    hw9

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,4 +1,44 @@
-// Описаний в документації
-import SimpleLightbox from "simplelightbox";
-// Додатковий імпорт стилів
-import "simplelightbox/dist/simple-lightbox.min.css";
+let formData = {
+    email: "",
+    message: ""
+};
+const formArea = document.querySelector(".feedback-form");
+
+const saveLS = () => {
+    try {
+        if (localStorage.length === 0) {
+            return;
+        }
+        const returnValue = JSON.parse(localStorage.getItem("feedback-form-state"));
+        for (const key in returnValue) {
+            formArea.elements[key].value = returnValue[key];
+        }
+        formData = returnValue;
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+saveLS();
+
+const onFormField = event => {
+    const inputValue = event.target.value;
+    const nameValue = event.target.name;
+    formData[nameValue] = inputValue;
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+};
+formArea.addEventListener("change", onFormField);
+const subActive = event => {
+    event.preventDefault()
+    localStorage.removeItem("feedback-form-state");
+    formArea.reset()
+}
+formArea.addEventListener("submit", subActive)
+
+
+
+
+
+
+
+
